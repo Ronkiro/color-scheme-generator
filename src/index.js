8d322e7e251d508fbd136eca71c557c5bd5ce4eb
Alexander Diniz <automaalex@hotmail.com>
2023-01-23 13:40:20 -0300

    feat: add more tools

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import 'toastr/build/toastr.min.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import "toastr/build/toastr.min.css";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
+    <a className="moar-tools" href="https://ronkiro.github.io/tools" target="__blank">
+      <div>
+        <div>
+          <i className="fa fa-bolt"></i>
+        </div>
+        <div>More Tools!</div>
+      </div>
+    </a>
+
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
